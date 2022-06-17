8f7e7acadb6833c40fc33a5d2c51003a73848f15
Amit Kumar <shakya94ak@gmail.com>
2022-06-17 20:29:36 +0530

    node

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 // 1.
-let a = parseInt(process.argv[2]);
-for (i = 1; i<=a; i++) {
-  console.log(i);
-}
+// let a = parseInt(process.argv[2]);
+// for (i = 1; i<=a; i++) {
+//   console.log(i);
+// }
 
 // 2.
-for (i = 1; i <= a; i++) {
-  if (i % 2 == 0) {
-    console.log(i);
-  }
-}
+// for (i = 1; i <= a; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// }
 
 // 3.
-let reverse = 0;
-let num = 48453;
-let d;
-while (num != 0) {
-  reverse = reverse * 10 + d;
-  num = Math.floor(num / 10);
-}
-console.log(reverse);
+// let reverse = 0;
+// let num = 48453;
+// let d;
+// while (num != 0) {
+//   reverse = reverse * 10 + d;
+//   num = Math.floor(num / 10);
+// }
+// console.log(reverse);
+
+// 1.
+// let i = 1;
+// while (i <= 100) {
+//   console.log(i);
+//   i++;
+// }
+
+// 2.
+// let i = 1;
+// while(i<=100) {
+// if(i % 2 == 0) {
+// console.log(i);
+// }
+// i++;
+// }
+
+// 3.
